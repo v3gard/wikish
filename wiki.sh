25f3f37780ad3e31a340af0718ef1457110ca6c8
Vegard Haugland <vegard@haugland.at>
2011-10-18 22:14:54 +0200

    Added sample config to wiki.sh and LIST command
     - Added sample config to wiki.sh. This is automatically written to
       ~/.config/wiki.conf at the first run.
     - Removed XDG base dir config. Yes, it was an ugly (unnecessary) beast ;-)
     - Added LIST command. Example: wiki.sh LIST a*
     - Renamed wikish.config to wikish.conf
     - Now checks if /usr/bin/GET is available. If it is not present,
       /usr/bin/lynx is used as a replacement. /usr/bin/GET is not a
       standard application in Slackware GNU/Linux, so I have added a check
       to see if /usr/bin/lynx is availble instead.

--- wiki.sh
+++ wiki.sh
@@ -3,7 +3,10 @@
 # Copyright (C) 2010 Redpill Linpro AS
 # Copyright (C) 2010 Kristian Lyngstol
 # Author: Kristian Lyngstøl <kristian@bohemians.org>
-# 
+#
+# Additonal contributions by:
+# Vegard Haugland <vegard@haugland.at>
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -18,16 +21,86 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+################
+# "Global variables" that are not really affected by configuration
+################
+
+TIM=$(date +%s)
+ME=$(basename $0)
+ACTION=""
+PAGE=""
+CONFIGDIR=~/.config
+CONFIGNAME=wikish.conf
+
+writeconfig()
+{
+    CONFIG=${CONFIGDIR}/${CONFIGNAME}
+    if [ -e ${CONFIG} ]; then
+        return
+    fi
+    if [ ! -d ${CONFIGDIR} ]; then
+        mkdir ${CONFIGDIR}
+    fi
+    cat > $CONFIG << _EOF_
+#!/bin/sh
+
+# Avoid trashing \$PWD by using a tmp dir:
+#cd /home/kristian/tmp/wiki
+# (now THAT'S simple)
+
+# Protocol to use (http or https)
+PROTO="http"
+
+# Hostname(+port)
+HOST="localhost"
+
+# ScriptPath
+#
+# Example:
+# http://path.to/w/index.php?title=Somepage&action=raw
+# where ScriptPath is "/w/index.php?title="
+SCRIPTPATH="/w/index.php?title="
+
+# API-url, with trailing ?
+#
+# Example (works for wiki.varnish-software.com):
+API="w/api.php?"
+
+# Example (works for wiki.redpill-linpro.com):
+# API="api.php?"
+
+USER="username"
+
+# Feel free to replace this with something smarter
+echo password:
+read -s PASSWORD
+
+# Like:
+# PASSWORD=password
+# or: 
+# PASSWORD=\$(gpg --decrypt ~/Documents/Passwords/wikipw)
+# ... you get the point. It's a shell script.
+_EOF_
+
+cat << _EOF_
+Note:
+
+A config file has been written to ~/.config/wikish.conf.
+Make the necessary changes, and run this script again.
+_EOF_
+exit 1
+}
 
 # Should probably >&2
 usage()
 {
-	cat <<_EOF_
+    cat <<_EOF_
 On a happier note:
 
 Configure stuff in wikish.config, then: 
  ./wiki.sh GET Some_Page
  ./wiki.sh POST Some_Page
+ ./wiki.sh LIST Som*
  ./wiki.sh EDIT Some_Page
  ./wiki.sh CLEAN
 (Any trailing .wiki in the page-name is stripped)
@@ -43,75 +116,73 @@ _EOF_
 }
 
 debug() {
-	if [ -z "$DEBUG" ]; then
-		true;
-	else
-		echo $*
-	fi
+    if [ -z "$DEBUG" ]; then
+        true;
+    else
+        echo $*
+    fi
 }
-
-################
-# "Global variables" that are not really affected by configuration
-################
-
-TIM=$(date +%s)
-ME=$(basename $0)
-ACTION=""
-PAGE=""
 
 ################
 # Configuration
 ################
-# Borrowed from compiz-manager: XDG base dirs is an ugly beast but I like
-# to do this just to remind people that you do not need a library to handle
-# this ugly mess. (I'm looking at you, awesome).
+#
+# Does config file exist? If not, create one.
+writeconfig
 
-# Read configuration from XDG paths
-NAME=wikish.config
-if [ -z "$XDG_CONFIG_DIRS" ]; then
-	test -f /etc/xdg/$NAME && . /etc/xdg/$NAME
+if [ -e "${CONFIGDIR}/${CONFIGNAME}" ]; then
+    test -f $CONFIGDIR/$CONFIGNAME && . ${CONFIGDIR}/${CONFIGNAME}
 else
-	test -f $XDG_CONFIG_DIRS/$NAME && . $XDG_CONFIG_DIRS/$NAME
+    echo "Config file not found. This should not happen..."
+    exit
 fi
 
-if [ -z "$XDG_CONFIG_HOME" ]; then
-	test -f $HOME/.config/$NAME && . $HOME/.config/$NAME
-else
-	test -f $XDG_CONFIG_HOME/$NAME && .  $XDG_CONFIG_HOME/$NAME
-fi
-
+# Check that the required variables are set in the config file
 if [ -z "$API" ] || [ -z "$USER" ] || [ -z "$HOST" ] || \
-	[ -z "$PASSWORD" ] || [ -z "$PROTO" ]; then
-	echo "Insufficient or missing configuration."
-	echo "Copy $NAME to an xdg-path (typically ~/.config/, see \$XDG_CONFIG_DIRS)"
-	exit 1;
+    [ -z "$PASSWORD" ] || [ -z "$PROTO" ] || [ -z "$SCRIPTPATH" ]; then
+    echo "Insufficient or missing configuration."
+    exit 1;
 fi
 
 # Read local config
-test -f $PWD/.wikish.config && . $PWD/.wikish.config
+test -f $PWD/.${CONFIGNAME} && . $PWD/.${CONFIGNAME}
+
+###############
+# How do we perform a GET request on a HTML page from the terminal?
+###############
+if [ -x /usr/bin/GET ];
+then
+    GETCMD=GET
+elif [ -x /usr/bin/lynx ];
+then
+    GETCMD=lynx
+fi
 
 ################
 # Sym-link and argument mapping
 ################
 
 if [ $ME = "wikiedit" ]; then
-	ACTION="EDIT"
-	PAGE=$1
+    ACTION="EDIT"
+    PAGE=$1
 elif [ $ME = "wikiget" ]; then
-	ACTION="GET"
-	PAGE=$1
+    ACTION="GET"
+    PAGE=$1
 elif [ $ME = "wikipost" ]; then
-	ACTION="POST"
-	PAGE=$1
+    ACTION="POST"
+    PAGE=$1
+elif [ $ME = "wikilist" ]; then
+    ACTION="LIST"
+    PAGE=$1
 else
-	ACTION=$1
-	PAGE=$2
+    ACTION=$1
+    PAGE=$2
 fi
 
 
 if [ -z "$PAGE" ] && [ ! "x$1" = "xCLEAN" ]; then
-	usage;
-	exit 1;
+    usage;
+    exit 1;
 fi
 
 # Allows for Main_Page and Main_Page.wiki - ie: tab completion.
@@ -124,23 +195,42 @@ mkdir -p `dirname "${PAGE}"`
 # Safely backs up any existing $PAGE.wiki to $PAGE.wiki.$TIM
 getit()
 {
-	if [ -f "$PAGE.wiki" ]; then
-		debug "Moving $PAGE.wiki to $PAGE.wiki.$TIM"
-		if [ -f $PAGE.wiki.$TIM ]; then
-			echo "PANIC! already exist. Stop using loops!";
-			sleep 5;
-			exit 1;
-		fi
-		mv "$PAGE.wiki" "$PAGE.wiki.$TIM"
-	fi
+    if [ -f "$PAGE.wiki" ]; then
+        debug "Moving $PAGE.wiki to $PAGE.wiki.$TIM"
+        if [ -f $PAGE.wiki.$TIM ]; then
+            echo "PANIC! already exist. Stop using loops!";
+            sleep 5;
+            exit 1;
+        fi
+        mv "$PAGE.wiki" "$PAGE.wiki.$TIM"
+    fi
 
-	GET "${PROTO}://${USER}:${PASSWORD}@${HOST}/${PAGE}?action=raw" > "$PAGE.wiki"
-	if [ $? = 0 ]; then
-		debug "$PAGE.wiki created seemingly without errors! Phew.";
-	else
-		echo "$PAGE.wiki GET-operation may have blown apart.  Abandon ship!"
-		exit 2;
-	fi
+    # Configure GETCMD according to available tools
+    case "$GETCMD" in
+      "GET")
+        GET "${PROTO}://${USER}:${PASSWORD}@${HOST}${SCRIPTPATH}${PAGE}&action=raw" > "$PAGE.wiki"
+      ;;
+      "lynx")
+        lynx -dump -auth "${USER}:${PASSWORD}" "${HOST}${SCRIPTPATH}${PAGE}&action=raw" > "$PAGE.wiki"
+      ;;
+    esac
+
+    if [ $? = 0 ]; then
+        debug "$PAGE.wiki created seemingly without errors! Phew.";
+    else
+        echo "$PAGE.wiki GET-operation may have blown apart.  Abandon ship!"
+        exit 2;
+    fi
+}
+
+# Connects to api.php and attempts to list pages with name given.
+listit()
+{
+     BURL="${PROTO}://${USER}:${PASSWORD}@${HOST}/${API}"
+     # in case the user lists "a*", remove "*"
+     PAGE=$( echo $PAGE | sed 's/*//g')
+     curl -s "${BURL}action=query&list=allpages&format=txt&aplimit=50&apprefix=${PAGE}" \
+         | grep title | sed 's#.*=> \(.*\)#\1#;s# #_#g'
 }
 
 # Gets an edittoken and session for editing $PAGE and posts the local
@@ -148,32 +238,42 @@ getit()
 # piped to sh. Not terribly pretty.
 postit()
 {
- 	BURL="${PROTO}://${USER}:${PASSWORD}@${HOST}/${API}"
-	# Welcome to the school of funky shell-nesting.
-	{	
- 		curl -s -c cookie.jar "${BURL}action=query&format=txt&prop=info&intoken=edit&title=$PAGE&titles=$PAGE" | awk -v page="$PAGE" -v burl="$BURL" '
-		BEGIN {
-			edittoken="";
-		}
-		/\[edittoken\] => / {
-			edittoken = $3;
-		}
-		END {
-			gsub("\\\\","\\\\",edittoken);
-			gsub("+","%2B",edittoken);
-			title=page
-			gsub(" ","%20",title);
-			printf "echo | curl -s --post301 -k --data-urlencode \"text@"
-			printf "%s.wiki\" ", page;
-			printf "-b cookie.jar";
-			printf " \"" burl "format=txt&action=edit&title=%s&titles=%s&token=%s\" \n", title, title, edittoken
-		}
-		' | sh
-		if [ ! $? = "0" ]; then
-			echo "Failed to push. curl returned non-zero" >&2;
-			exit 1;
-		fi
-	} | egrep '(result|title)'
+    # Check if the file is UTF-8. If so, convert to ASCII.
+    # If we send a UTF-8 encoded file to the MediaWiki API, the wiki syntax
+    # will not be parsed and the browser will display the raw wiki text in a
+    # neat (unparsed) oneliner.
+
+    if [ -n "$( file $PAGE.wiki | grep "UTF-8" )" ]; then
+        awk '{if(NR==1)sub(/^\xef\xbb\xbf/,"");print}' $PAGE.wiki > $PAGE.wiki.fixed
+        mv $PAGE.wiki.fixed $PAGE.wiki
+    fi
+
+     BURL="${PROTO}://${USER}:${PASSWORD}@${HOST}/${API}"
+    # Welcome to the school of funky shell-nesting.
+    {    
+         curl -s -c cookie.jar "${BURL}action=query&format=txt&prop=info&intoken=edit&title=$PAGE&titles=$PAGE" | awk -v page="$PAGE" -v burl="$BURL" '
+        BEGIN {
+            edittoken="";
+        }
+        /\[edittoken\] => / {
+            edittoken = $3;
+        }
+        END {
+            gsub("\\\\","\\\\",edittoken);
+            gsub("+","%2B",edittoken);
+            title=page
+            gsub(" ","%20",title);
+            printf "echo | curl -s --post301 -k --data-urlencode \"text@"
+            printf "%s.wiki\" ", page;
+            printf "-b cookie.jar";
+            printf " \"" burl "format=txt&action=edit&title=%s&titles=%s&token=%s\" \n", title, title, edittoken
+        }
+        ' | sh
+        if [ ! $? = "0" ]; then
+            echo "Failed to push. curl returned non-zero" >&2;
+            exit 1;
+        fi
+    } | egrep '(result|title)'
 }
 
 
@@ -182,38 +282,40 @@ postit()
 ################
 
 if [ "x$ACTION" = "xGET" ]; then
-	getit
+    getit
 elif [ "x$ACTION" = "xPOST" ]; then
-	postit
+    postit
+elif [ "x$ACTION" = "xLIST" ]; then
+    listit
 elif [ "x$ACTION" = "xEDIT" ]; then
-	getit
-	cp "$PAGE.wiki" "$PAGE.wiki.original.$TIM"
-	# Thank Red Hat for the non-vi-clone support: they keep /bin/vi
-	# bastardized so you need/want to run 'vim' explicitly. Otherwise
-	# there would be no reason to support other editors.
-	if [ -z "$EDITOR" ]; then
-		vi "$PAGE.wiki"
-	else
-		$EDITOR "$PAGE.wiki"
-	fi
-	diff -q "$PAGE.wiki" "$PAGE.wiki.original.$TIM" >/dev/null && {
-		echo "Unchanged - not pushing it."
-		exit 1;
-	}
-	postit
+    getit
+    cp "$PAGE.wiki" "$PAGE.wiki.original.$TIM"
+    # Thank Red Hat for the non-vi-clone support: they keep /bin/vi
+    # bastardized so you need/want to run 'vim' explicitly. Otherwise
+    # there would be no reason to support other editors.
+    if [ -z "$EDITOR" ]; then
+        vi "$PAGE.wiki"
+    else
+        $EDITOR "$PAGE.wiki"
+    fi
+    diff -q "$PAGE.wiki" "$PAGE.wiki.original.$TIM" >/dev/null && {
+        echo "Unchanged - not pushing it."
+        exit 1;
+    }
+    postit
 elif [ "x$ACTION" = "xCLEAN" ]; then
-	# Generate too much crap. One day I will put it in dot-files.
-	RMS=*.wiki.*[0-9]*
-	echo $RMS
-	echo "Shall I kill the above files?"
-	echo "[Y]es/No!"
-	read yesno
-	if [ -z "$yesno" ] || [ "$yesno" = "Y" ] || [ "$yesno" = "y" ] || [ "x$yesno" = "xyes" ]; then
-		rm $RMS && echo "Done"
-	else
-		echo "Bailing!";
-	fi
+    # Generate too much crap. One day I will put it in dot-files.
+    RMS=*.wiki.*[0-9]*
+    echo $RMS
+    echo "Shall I kill the above files?"
+    echo "[Y]es/No!"
+    read yesno
+    if [ -z "$yesno" ] || [ "$yesno" = "Y" ] || [ "$yesno" = "y" ] || [ "x$yesno" = "xyes" ]; then
+        rm $RMS && echo "Done"
+    else
+        echo "Bailing!";
+    fi
 else
-	echo "Unknown arguments" >&2
-	usage
+    echo "Unknown arguments" >&2
+    usage
 fi
